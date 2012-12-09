25ed732300755dd134befcff5941cc4fa9730d46
Noboru Koike <etthimawataru@gmail.com>
2012-12-10 00:54:58 +0900

    Modified ...

--- iketrial/trial/models.py
+++ iketrial/trial/models.py
@@ -2,10 +2,15 @@
 from google.appengine.ext import db
 
 class ModelInput1(db.Model):
-	value = db.StringProperty(verbose_name='値', multiline=True, required=True)
+	value = db.StringProperty(verbose_name='value', multiline=True, required=True)
 
 class ModelInput2(db.Model):
-	value = db.StringProperty(verbose_name='値', multiline=True, required=True)
+	value = db.StringProperty(verbose_name='value', multiline=True, required=True)
 
 class ModelInput3(db.Model):
-	value = db.StringProperty(verbose_name='値', multiline=True, required=True)
+	value = db.StringProperty(verbose_name='value', multiline=True, required=True)
+
+class TrialPocketAccount(db.Model):
+	api_key = db.StringProperty(required=True)
+	user_name = db.StringProperty(required=True)
+	encrypted_password = db.StringProperty(required=True)
